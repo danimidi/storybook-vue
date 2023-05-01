677c5784af5a8ce1688ed0a5f4d6667e1e2faf56
danimidi <nelsi.miramag@hunty.com>
2023-04-30 22:51:23 -0500

    feat: add index.js

--- src/main.js
+++ src/main.js
@@ -1,16 +1,3 @@
-import { createApp } from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
-
-import axios from 'axios';
-import VueAxios from 'vue-axios';
-
-import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
-
-const app = createApp(App);
-
-app.use(store).use(router).use(VueAxios, axios).use(BootstrapIconsPlugin);
-app.mount('#app');
-
 import './main.css';
+
+export * from './components';
