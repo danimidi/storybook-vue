9a3560263b50d5b994f1677bb36d24ab61ea8657
nelsimiramag <nelsi.miramag@hunty.com>
2023-07-11 15:40:04 -0500

    feat: add icon to table

--- src/components/Table/Table.vue
+++ src/components/Table/Table.vue
@@ -1,11 +1,16 @@
 <script>
+import { BIconDownload } from 'bootstrap-icons-vue';
+
 export default {
   name: 'Table',
   props: {},
-  computed: {},
+  components: { BIconDownload },
 };
 </script>
 
 <template>
-  <div>Table test</div>
+  <div class="sk-flex sk-gap-2">
+    <p>Table test</p>
+    <BIconDownload class="sk-h-5 sk-w-5"></BIconDownload>
+  </div>
 </template>
